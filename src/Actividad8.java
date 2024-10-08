4054e8b56d22f0547f13b61a266c55dd2c6252c5
Carlos-Gamrra554 <cargamtom@alu.edu.gva.es>
2024-10-08 10:17:15 +0200

    Actividad 8

--- src/Actividad8.java
+++ src/Actividad8.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 public class Actividad8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean correcto = false;
 
-        System.out.print("Introduce un número: ");
-        int num;
-
-        try {
-          num = sc.nextInt();
-            System.out.println("Opción válida!");
-        } catch (InputMismatchException e){
-            System.out.println("Solo se permiten números!");
-            e.printStackTrace();
+        while (!correcto) {
+            try {
+                System.out.print("Introduce un número: ");
+                int num = sc.nextInt();
+                System.out.println("Opción válida!");
+                correcto = true;
+            } catch (InputMismatchException e) {
+                //e.printStackTrace();
+                System.out.println("Solo se permiten números!");
+                sc.nextLine();
+            }
         }
-
     }
 }
